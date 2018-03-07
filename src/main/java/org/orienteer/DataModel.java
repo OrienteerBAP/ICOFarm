b5bdcfb38a713ef3a891e343b6e32c3bd642692a
Vitaliy Gonchar <weaxme@gmail.com>
2018-03-07 22:24:47 +0200

    Initial commit

---
+++ src/main/java/org/orienteer/DataModel.java
@@ -0,0 +1,26 @@
+package org.orienteer;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.AbstractOrienteerModule;
+import org.orienteer.core.util.OSchemaHelper;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+
+public class DataModel extends AbstractOrienteerModule{
+
+	protected DataModel() {
+		super("ICOFarm", 1);
+	}
+	
+	@Override
+	public ODocument onInstall(OrienteerWebApplication app, ODatabaseDocument db) {
+		super.onInstall(app, db);
+		OSchemaHelper helper = OSchemaHelper.bind(db);
+		//Install data model
+		//Return null of default OModule is enough
+		return null;
+	}
+	
+}
---
+++ src/main/java/org/orienteer/MyWebApplication.java
@@ -0,0 +1,16 @@
+package org.orienteer;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPages("org.orienteer.web");
+		registerModule(DataModel.class);
+	}
+	
+}
