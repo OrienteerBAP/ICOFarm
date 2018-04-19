95301bbea15eeb48ba7392346bebdceaf82b6f46
Vitaliy Gonchar <weaxme@gmail.com>
2018-04-19 14:06:37 +0300

    Refactor TranferToken method and TransferTokenPanel
    1. Renamed TransferTokenPopupPanel to TranferTokenPanel
    2. Refactor TransferToken by using new version of TransferTokenPanel

---
+++ src/main/java/org/orienteer/component/TransferTokenPanel.java
@@ -0,0 +1,92 @@
+package org.orienteer.component;
+
+import com.google.inject.Inject;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
+import org.orienteer.model.Token;
+import org.orienteer.model.Wallet;
+import org.orienteer.service.web3.IEthereumService;
+import org.web3j.crypto.Credentials;
+
+import java.math.BigInteger;
+
+public class TransferTokenPanel extends AbstractTokenPanel {
+
+	@Inject
+	private IEthereumService service;
+
+	public TransferTokenPanel(String id, IModel<Wallet> walletModel, IModel<Token> tokenModel) {
+		super(id, walletModel, tokenModel);
+	}
+
+	@Override
+	protected void onInitialize(Form<?> form) {
+		TextField<String> target = new TextField<>("target", Model.of());
+		target.setRequired(true);
+		form.add(new Label("targetLabel", new ResourceModel("transfer.token.target.wallet")));
+		form.add(target);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	protected void onFormSubmit(AjaxRequestTarget target, Form<?> form) {
+		try {
+			String password = ((TextField<String>) form.get("password")).getModelObject();
+			String targetAddress = ((TextField<String>) form.get("target")).getModelObject();
+			long quantity = ((TextField<Long>) form.get("quantity")).getModelObject();
+			transferTokens(password, quantity, targetAddress);
+
+			onTransferTokens(target);
+		} catch (Exception ex) {
+			error(new ResourceModel("transfer.token.error").getObject());
+		}
+	}
+
+	private void transferTokens(String password, long quantity, String target) throws Exception {
+		Token token = getTokenModel().getObject();
+		Wallet wallet = getWalletModel().getObject();
+		Credentials credentials = service.readWallet(password, wallet.getWalletJSON());
+		BigInteger gasPrice = token.getGasPrice().toBigInteger();
+		BigInteger gasLimit = token.getGasLimit().toBigInteger();
+		service.transferTokens(credentials, token.getAddress(), target, BigInteger.valueOf(quantity), gasPrice, gasLimit);
+	}
+
+	protected void onTransferTokens(AjaxRequestTarget target) {
+
+	}
+
+	@Override
+	protected IModel<String> getTitleModel() {
+		return new ResourceModel("transfer.token.title");
+	}
+
+	@Override
+	protected IModel<String> getPasswordLabelModel() {
+		return new ResourceModel("transfer.token.password");
+	}
+
+	@Override
+	protected IModel<String> getSelectWalletLabelModel() {
+		return new ResourceModel("transfer.token.select.wallet");
+	}
+
+	@Override
+	protected IModel<String> getSelectTokenLabelModel() {
+		return new ResourceModel("transfer.token.select.token");
+	}
+
+	@Override
+	protected IModel<String> getQuantityLabelModel() {
+		return new ResourceModel("transfer.token.quantity");
+	}
+
+	@Override
+	protected IModel<String> getSubmitBtnLabelModel() {
+		return new ResourceModel("transfer.token.submit");
+	}
+}
--- src/main/java/org/orienteer/method/TransferToken.java
+++ src/main/java/org/orienteer/method/TransferToken.java
@@ -4,9 +4,8 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
-import org.apache.wicket.model.IModel;
-import org.orienteer.ICOFarmApplication;
-import org.orienteer.component.TransferTokenPopupPanel;
+import org.apache.wicket.model.Model;
+import org.orienteer.component.TransferTokenPanel;
 import org.orienteer.core.component.FAIconType;
 import org.orienteer.core.component.command.AbstractModalWindowCommand;
 import org.orienteer.core.method.OFilter;
@@ -14,87 +13,34 @@ import org.orienteer.core.method.OMethod;
 import org.orienteer.core.method.filters.ODocumentFilter;
 import org.orienteer.core.method.filters.PlaceFilter;
 import org.orienteer.core.method.methods.AbstractModalOMethod;
-import org.orienteer.model.EthereumClientConfig;
 import org.orienteer.model.Wallet;
-import org.orienteer.service.web3.IEthereumService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.web3j.crypto.Credentials;
-import org.web3j.utils.Convert;
-import ru.ydn.wicket.wicketorientdb.model.SimpleNamingModel;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
 
 @OMethod(
 		icon = FAIconType.external_link,
 		titleKey = "withdraw",
 		filters = {
-			@OFilter(fClass = ODocumentFilter.class, fData = "Wallet"),
+			@OFilter(fClass = ODocumentFilter.class, fData = Wallet.CLASS_NAME),
 			@OFilter(fClass = PlaceFilter.class, fData = "STRUCTURE_TABLE"),
 		}
 )
 public class TransferToken extends AbstractModalOMethod {
-	private static final long serialVersionUID = 1L;
 
-	private static final Logger LOG = LoggerFactory.getLogger(BuyToken.class);
-
-	// TODO: refactor static fields
-	private static final BigInteger GAS_PRICE = Convert.toWei(BigDecimal.ONE, Convert.Unit.GWEI).toBigInteger();
-	private static final BigInteger GAS_LIMIT = BigInteger.valueOf(200000);
-	
 	@Override
 	public Component getModalContent(String componentId, ModalWindow modal,AbstractModalWindowCommand<?> command) {
-		return new TransferTokenPopupPanel(componentId, modal,command) {
-			private static final long serialVersionUID = 1L;
+		modal.setMinimalWidth(370);
+		modal.setAutoSize(true);
 
+		return new TransferTokenPanel(componentId, Model.of(getWallet()), Model.of()) {
 			@Override
-			public boolean onSubmitForm(AjaxRequestTarget target) {
-//				try {
-//					IModel<String> password = getWalletPassword();
-//					IModel<String> summ = getTokenQuantity();
-//					IModel<String> targetWallet = getTargetWalletAddress();
-//					String tokenAddress = getToken().getContractAddress();
-//					Credentials credentials = getCredentials(password.getObject(), getWallet());
-//
-//					getEthereumService().transferTokens(credentials, tokenAddress, targetWallet.getObject(), new BigInteger(summ.getObject()), GAS_PRICE, GAS_LIMIT);
-//					//https://rinkeby.etherscan.io/address/0xf8f3d3d326c78f0d274f91f2428305a89002660e
-//					//AbstractWidgetDisplayModeAwarePage<ODocument> page = new ODocumentPage(new ODocumentModel(session.getOTaskSessionPersisted().getDocument())).setModeObject(DisplayMode.VIEW);
-//					return true;
-//				} catch (Exception e) {
-//					LOG.error("Can't transfer tokens!", e);
-//					error(e.getMessage()+" ");
-//				}
-				return false;
+			protected void onTransferTokens(AjaxRequestTarget target) {
+				modal.close(target);
+				command.onAfterModalSubmit();
 			}
-
-			@Override
-			public SimpleNamingModel<String> getButtonTitle() {
-				return getTitleModel();
-			}
-
 		};
 	}
 
-	protected Wallet getWallet() throws Exception {
-		IModel<?> walletModel = getContext().getDisplayObjectModel();
-		ODocument walletDoc = (ODocument) walletModel.getObject();
-		if (walletDoc == null) {
-			throw new Exception("Please link buy button to 'EthereumWallet' OClass");
-		}
+	protected Wallet getWallet() {
+		ODocument walletDoc = (ODocument) getContext().getDisplayObjectModel().getObject();
 		return new Wallet(walletDoc);
-		
-	}
-
-	protected EthereumClientConfig getConfig(){
-		return getEthereumService().getConfig();
-	}
-
-	protected IEthereumService getEthereumService() {
-		return ICOFarmApplication.get().getInjector().getInstance(IEthereumService.class);
-	}
-
-	private Credentials getCredentials(String password, Wallet wallet) throws Exception {
-		return getEthereumService().readWallet(password, wallet.getWalletJSON());
 	}
 }
