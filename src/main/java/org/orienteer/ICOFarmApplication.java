70d60ef9cb4ccaf6cddb7d4c6fac0589b728c626
Vitaliy Gonchar <weaxme@gmail.com>
2018-04-30 03:39:54 +0300

    Removed transaction hook

--- src/main/java/org/orienteer/ICOFarmApplication.java
+++ src/main/java/org/orienteer/ICOFarmApplication.java
@@ -40,7 +40,6 @@ public class ICOFarmApplication extends OrienteerWebApplication {
 		List<Class<? extends ORecordHook>> hooks = getOrientDbSettings().getORecordHooks();
 		hooks.add(OWidgetHook.class);
 		hooks.add(WalletHook.class);
-		hooks.add(OTransactionHook.class);
 	}
 
 	@Override
